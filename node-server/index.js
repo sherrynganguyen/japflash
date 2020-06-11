b20017fb06561d6056beef0492238050082158a6
Nga Nguyen <ngasdragon@gmail.com>
2020-06-10 23:28:51 -0700

    Connected to mongodb

--- node-server/index.js
+++ node-server/index.js
@@ -1,9 +1,23 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8080;
+const mongoose = require('mongoose');
 
+const PORT = process.env.PORT || 8080;
+const DBUSER = process.env.DBUSER;
+const DBPASSWORD = process.env.DBPASSWORD;
+
+const app = express();
+
+const url = `mongodb+srv://${DBUSER}:${DBPASSWORD}@cluster0-onasa.mongodb.net/japflash?retryWrites=true&w=majority`;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true
+}, (err) => {
+  (!err) ? console.log('Connected successfully to database') : console.log(`Cannot connect to database due to ${err}`);
+})
 app.use('/', (req,res) => {
   res.send('Welcom to JapFlash');
 });
