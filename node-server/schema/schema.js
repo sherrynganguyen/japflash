1f12b42a2e7c952b1426b77b303d06d8d6ed408d
Nga Nguyen <ngasdragon@gmail.com>
2020-06-11 01:07:37 -0700

    mutation add data to mongodb database

--- node-server/schema/schema.js
+++ node-server/schema/schema.js
@@ -9,7 +9,8 @@ const {
   GraphQLString,
   GraphQLID,
   GraphQLList,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLNonNull
 } = graphql;
 
 const words = [
@@ -34,8 +35,9 @@ const WordType = new GraphQLObjectType({
     categoryId: {
       type: CategoryType,
       resolve(parent, args){
-        return _.find(categories, {id: parent.categoryId})
-        // return Author.findById(parent.authorId)
+        // return _.find(categories, {id: parent.categoryId})
+
+        return Category.findById(parent.categoryId)
       }
     }
   })
@@ -45,12 +47,12 @@ const CategoryType = new GraphQLObjectType({
   name: 'Category',
   fields: () => ({
     id:  { type: GraphQLID },
-    type: { type: GraphQLString },
+    name: { type: GraphQLString },
     words: {
       type: new GraphQLList(WordType),
       resolve(parent, args){
-        return _.filter(words, {categoryId: parent.id})
-        // return Author.findById(parent.authorId)
+        // return _.filter(words, {categoryId: parent.id})
+        return Word.find({categoryId: parent.id})
       }
     }
   })
@@ -63,20 +65,65 @@ const RootQuery = new GraphQLObjectType({
       type:WordType,
       args: {id: {type: GraphQLID}},
       resolve(parent, args){
-        return _.find(words, {id: args.id})
+        return Word.findById(args.id)
       }
     },
     category: {
       type:CategoryType,
       args: {id: {type: GraphQLID}},
       resolve(parent, args){
-        return _.find(categories, {id: args.id})
+        return Category.findById(args.id)
+      }
+    },
+    words: {
+      type: new GraphQLList(WordType),
+      resolve(parent,args){
+        return Word.find({})
+      }
+    },
+    categories: {
+      type: new GraphQLList(CategoryType),
+      resolve(parent,args){
+        return Category.find({})
       }
     }
   }
 });
 
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addWord: {
+      type: WordType,
+      args: {
+        eng: {type: new GraphQLNonNull(GraphQLString)},
+        jap: {type: new GraphQLNonNull(GraphQLString)},
+        categoryId: {type: new GraphQLNonNull(GraphQLID)},
+      },
+      resolve(parent,args) {
+        let word = new Word({
+          eng: args.eng,
+          jap: args.jap,
+          categoryId: args.categoryId
+        })
+        return word.save();
+      }
+    },
+    addCategory: {
+      type: CategoryType,
+      args: {
+        name: {type: new GraphQLNonNull(GraphQLString)},
+      },
+      resolve(parent,args) {
+        let category = new Category({
+          name: args.name,
+        })
+        return category.save();
+      }
+    }
+  }
+})
 module.exports = new GraphQLSchema({
   query: RootQuery,
-  // mutation: Mutation
+  mutation: Mutation
 })
