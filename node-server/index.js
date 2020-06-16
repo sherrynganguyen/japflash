8c0e5936d13d1695fe8fc42636dd30c984071bac
Nga Nguyen <ngasdragon@gmail.com>
2020-06-16 01:16:25 -0700

    Deploy to heroku

--- node-server/index.js
+++ node-server/index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const cors = require('cors');
+const path = require('path');
 
 const PORT = process.env.PORT || 8080;
 const DBUSER = process.env.DBUSER;
@@ -22,6 +23,13 @@ mongoose.connect(url, {
 }, (err) => {
   (!err) ? console.log('Connected successfully to database') : console.log(`Cannot connect to database due to ${err}`);
 });
+
+app.use(express.static(path.join(__dirname, '../react-frontend/public')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../react-frontend/public/index.html'))
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
