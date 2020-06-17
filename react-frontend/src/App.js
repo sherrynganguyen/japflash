4cf5a817001e3050dcc958c517a068d584405a50
Nga Nguyen <ngasdragon@gmail.com>
2020-06-16 23:20:56 -0700

    Revert "working on navbar"
    This reverts commit ad5c996f1618fd873aa25528709e1fbd512e0698.

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -11,7 +11,6 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import AddNewWord from './components/addNewWord';
 import FlashCards from './components/flashCards';
 import MainPage from './components/mainPage';
-import NavBar from './components/navbar';
 
 const client = new ApolloClient({
   uri: 'http://localhost:8080/graphql',
@@ -23,10 +22,6 @@ function App() {
       <Router>
         <Switch>
           <ApolloProvider client={client}>
-            <div className="header">
-              <NavBar/>
-              <h1 className="title">JapFlash</h1>
-            </div>
             <Route exact path="/" component={MainPage}></Route>
             <Route path="/input" component={AddNewWord}></Route>
             <Route path="/flashcards" component={FlashCards}></Route>
--- react-frontend/src/components/mainPage.js
+++ react-frontend/src/components/mainPage.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import NavBar from './navbar';
-import './styles.scss';
+import {
+  Link
+} from 'react-router-dom';
+
 export default function MainPage() {
   return (
-    <div className="main-page">
-      
-      <NavBar/>
-      {/* <nav className="navbar">
-      <ul>
+    <>
+      <h1 className="title">JapFlash</h1>
+      <nav className="navbar">
         <Link to="/input">Input</Link>
         <Link to="/flashcards">FlashCards</Link>
         <Link to="/challenge">Challenge</Link>
         <Link to="/multiple">Multiple</Link>
         <Link to="/multiple">Report</Link>
-      </ul>
-    </nav> */}
-    </div>
+      </nav>
+    </>
   )
 }
