d730f927fb2ca8bbe13d4684b9ae0fa4f0ef1594
Nga Nguyen <ngasdragon@gmail.com>
2020-07-27 15:55:09 -0700

    get swipe feature working

--- react-frontend/src/components/FlashCards.js
+++ react-frontend/src/components/FlashCards.js
@@ -8,9 +8,20 @@ import { getWordQuery } from '../queries/queries';
 
 import './styles.scss';
 import Cards from './Cards';
+// import Deck from './Cards'
 import WordDetails from './WordDetails';
 
+
 export default function FlashCards() {
+  
+  const cards = [
+    'https://upload.wikimedia.org/wikipedia/en/f/f5/RWS_Tarot_08_Strength.jpg',
+    'https://upload.wikimedia.org/wikipedia/en/5/53/RWS_Tarot_16_Tower.jpg',
+    'https://upload.wikimedia.org/wikipedia/en/9/9b/RWS_Tarot_07_Chariot.jpg',
+    'https://upload.wikimedia.org/wikipedia/en/d/db/RWS_Tarot_06_Lovers.jpg',
+    'https://upload.wikimedia.org/wikipedia/en/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
+    'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg'
+  ]
   
   const WordsList = () => {
     const [selected, setSelected] = useState(null);
@@ -26,11 +37,12 @@ export default function FlashCards() {
     //   </div>
     // )
     return <Cards list={data.words}/>
+    // return <Deck cards={cards}/>
   }
 
   return (
-    <div>
-      <h2>Flash</h2>
+    <div className="flashCards">
+      {/* <h2>Flash</h2> */}
       {WordsList()}
     </div>
     
